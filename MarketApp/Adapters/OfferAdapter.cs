e33b532ebdbfbb8e6e60cea126fc62095b5b002e
Forcause <ykhrishtakyan@gmail.com>
2023-03-23 21:19:51 +0300

    Add project files.

---
+++ MarketApp/Adapters/OfferAdapter.cs
@@ -0,0 +1,31 @@
+﻿using Android.Views;
+using Android.Widget;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace MarketApp.Adapters
+{
+    internal class OfferAdapter : BaseAdapter<XmlElement>
+    {
+        private readonly IList<XmlElement> _offers;
+
+        public OfferAdapter(IList<XmlElement> offers)
+        {
+            _offers = offers;
+        }
+        public override XmlElement this[int position] => _offers[position];
+
+        public override int Count => _offers.Count;
+
+        public override long GetItemId(int position) => long.Parse(_offers[position].Attributes["id"].Value);
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            var view = LayoutInflater.From(parent.Context).Inflate(Resource.Layout.custom_row_layout, parent, false);
+            var id = view.FindViewById<TextView>(Resource.Id.offer_id);
+            id.Text = $"{GetItemId(position)}";
+
+            return view;
+        }
+    }
+}
---
+++ MarketApp/MainActivity.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using MarketApp.Adapters;
+using MarketApp.Services;
+using System.Text;
+using System.Xml;
+
+namespace MarketApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override async void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            var offersList = FindViewById<ListView>(Resource.Id.offerList);
+            var webService = new WebService();
+            var offers = await webService.GetOfferAsync();
+            
+            var adapter = new OfferAdapter(offers);
+            offersList.Adapter = adapter;
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
